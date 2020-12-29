b93b6c67a10ec3d60a2c986ea2b7e67e36446cdb
Karthik002 <karthikprasad62@gmail.com>
2020-12-29 18:25:23 -0500

    Revamp
    Initial movement of files to revamp the website. Adding landing page with "past competitions" linked to old website

---
+++ script.js
@@ -0,0 +1,17 @@
+var countDownDate = new Date("Feb 17, 2021 24:00:00").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                    
+    document.getElementById("compStart").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+                    
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "Registration Closed";
+    }
+}, 1000);
